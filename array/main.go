c6ae9c874453a359a3d73f9e0041588f8842871d
Phobphoomin Siriboon <90683904+prompto416@users.noreply.github.com>
2025-11-18 02:59:55 +0700

    Update main.go: Call ReverseArray

--- array/main.go
+++ array/main.go
@@ -1,15 +1,17 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	res1 := secondLargestOnlogn([]int{12, 35, 1, 10, 34, 1})
-	fmt.Println(res1,"hi")
+	// res1 := secondLargestOnlogn([]int{12, 35, 1, 10, 34, 1})
+	// fmt.Println(res1,"hi")
 
+	// res2 := secondLargestOn([]int{10, 5, 10})
+	// fmt.Println(res2)
 
-	res2 := secondLargestOn([]int{10, 5, 10})
-	fmt.Println(res2)
-	
+	// res3 := thirdLargest([]int{100,50,200})
+	// fmt.Println(res3)
+
+	fmt.Println(ReverseArray([]int{1, 2, 3, 4, 5,6}))
+
 }
